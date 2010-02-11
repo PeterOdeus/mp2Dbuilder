6f2a7eefd3aec236f5f274b5038b1d80fd75f859
Peter Odéus <peter.odeus@gmail.com>
2010-02-11 15:52:17 +0100

    Added Junit test that handles multiple smirks definition in input file

--- src/test/java/mp2dbuilder/builder/DataBuilderAppTest.java
+++ src/test/java/mp2dbuilder/builder/DataBuilderAppTest.java
@@ -46,4 +46,20 @@ public class DataBuilderAppTest {
 		                  reactionSmartsFilterFile};
 		DataBuilderApp.main(args);
 	}
+	
+	@Test public void testDataBuilderAppTwoSmirks()throws Exception{
+		URL url = this.getClass().getClassLoader().getResource("data/mdl/firstRiReg.rdf");
+		String inFile = url.getPath();
+		url = this.getClass().getClassLoader().getResource("data/mdl");
+		String outFile = url.getPath() + "/simpleSMIRKSout.bin";
+		url = this.getClass().getClassLoader().getResource("data/mdl/ReactionSMARTSFilter.two");
+		String reactionSmartsFilterFile = url.getPath();
+		String [] args = {"-i", 
+		                  inFile,
+		                  "-o",
+		                  outFile,
+		                  "-rfile",
+		                  reactionSmartsFilterFile};
+		DataBuilderApp.main(args);
+	}
 }
