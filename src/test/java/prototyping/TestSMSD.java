310aa0cd58164977f4337dcec176bd8e249c8d47
Ola Spjuth <ola.spjuth@gmail.com>
2010-02-24 23:01:09 +0100

    Read 500-bla file and calculate SMSD.

--- src/test/java/prototyping/TestSMSD.java
+++ src/test/java/prototyping/TestSMSD.java
@@ -1,13 +1,25 @@
 package prototyping;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.Map;
 import java.util.TreeMap;
 
+import org.openscience.cdk.Molecule;
+import org.openscience.cdk.Reaction;
 import org.openscience.cdk.aromaticity.CDKHueckelAromaticityDetector;
+import org.openscience.cdk.exception.CDKException;
 import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.interfaces.IChemObject;
 import org.openscience.cdk.interfaces.IMolecule;
+import org.openscience.cdk.interfaces.IReaction;
+import org.openscience.cdk.interfaces.IReactionSet;
+import org.openscience.cdk.io.ReaccsMDLRXNReader;
+import org.openscience.cdk.nonotify.NNChemObject;
+import org.openscience.cdk.nonotify.NNReactionSet;
 import org.openscience.cdk.nonotify.NoNotificationChemObjectBuilder;
 import org.openscience.cdk.smiles.SmilesParser;
 import org.openscience.cdk.smsd.SMSD;
@@ -18,14 +30,41 @@ public class TestSMSD {
 
 	
 	public static void main(String[] args) throws Exception {
+
+		String filename = "metaprint2d/data/First500DB2005AllFields.rdf"; //"data/mdl/73320thRiReg.rdf";
 		
-		SmilesParser sp = new SmilesParser(NoNotificationChemObjectBuilder.getInstance());
-//		IMolecule A1 = sp.parseSmiles("CNC(CC=OC)CCCCN(C)C");
-//		IMolecule A2 = sp.parseSmiles("CNC(CC=OC)CCCCN(C)C");
-		IMolecule A1 = sp.parseSmiles("CN(=C)O");
-		IMolecule A2 = sp.parseSmiles("CN(=C)O");
-		System.out.println("mol1: " + A1);
-		System.out.println("mol2: " + A1);
+		System.out.println("Testing: " + filename);
+		InputStream ins = TestSMSD.class.getClassLoader().getResourceAsStream(filename);
+		URL url = TestSMSD.class.getClassLoader().getResource(filename);
+		ReaccsMDLRXNReader reader = new ReaccsMDLRXNReader(ins);
+		File file = new File(url.toURI());
+		long fileLengthLong = file.length();
+
+		reader.activateReset(fileLengthLong);
+
+		//Read reaction
+		IReactionSet reactionSet = (IReactionSet)reader.read(new NNReactionSet());
+		IReaction reaction = reactionSet.getReaction(0);
+
+		System.out.println("Noreacs: " + reactionSet.getReactionCount());
+
+		int cnt=1;
+		System.out.print("Running SMSD on reaction: " + cnt);
+		while(reaction!=null){
+			runSMSD(reaction);
+			reactionSet = (IReactionSet)reader.read(new NNReactionSet());
+			reaction = reactionSet.getReaction(0);
+			cnt++;
+			System.out.print("," + cnt);
+		}
+		
+
+	}
+	
+	public static void runSMSD(IReaction reaction) throws Exception{
+		
+		IAtomContainer A1=reaction.getReactants().getAtomContainer(0);
+		IAtomContainer A2=reaction.getProducts().getAtomContainer(0);
 		
         AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(A1);
         AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(A2);
@@ -64,22 +103,22 @@ public class TestSMSD {
             //Get the mapped atom in Target Molecule
             IAtom targetAtom = Target.getAtom(targetMappingNumber);
             //Print mapped atom numbers
-            System.out.println(queryMappingNumber + " " +
-                    (targetMappingNumber));
-            //Print mapped atoms
-            System.out.println(queryAtom.getSymbol() + " " +
-                    targetAtom.getSymbol());
+//            System.out.println(queryMappingNumber + " " +
+//                    (targetMappingNumber));
+//            //Print mapped atoms
+//            System.out.println(queryAtom.getSymbol() + " " +
+//                    targetAtom.getSymbol());
         }
-        System.out.println("");
+//        System.out.println("");
+//
+//        System.out.println("");
 
-        System.out.println("");
-
-        System.out.println("Stereo Match: " + comparison.getStereoScore(0));
-        System.out.println("Stereo different: " + comparison.isStereoMisMatch());
-        System.out.println("Fragment Size: " + comparison.getFragmentSize(0));
-        System.out.println("Tanimoto Similarity Score: " + comparison.getTanimotoSimilarity());
-        System.out.println("Tanimoto Euclidean Distance: " + comparison.getEuclideanDistance());
-        System.out.println("");
+//        System.out.println("Stereo Match: " + comparison.getStereoScore(0));
+//        System.out.println("Stereo different: " + comparison.isStereoMisMatch());
+//        System.out.println("Fragment Size: " + comparison.getFragmentSize(0));
+//        System.out.println("Tanimoto Similarity Score: " + comparison.getTanimotoSimilarity());
+//        System.out.println("Tanimoto Euclidean Distance: " + comparison.getEuclideanDistance());
+//        System.out.println("");
 
         int nrMCSs = 0;
 		for (TreeMap<Integer, Integer> listOfMappings : comparison.getAllMapping()){
@@ -96,15 +135,18 @@ public class TestSMSD {
 	            //Get the mapped atom in Target Molecule
 	            IAtom targetAtom = Target.getAtom(targetMappingNumber);
 	            //Print mapped atom numbers
-	            System.out.println(queryMappingNumber + " " +
-	                    (targetMappingNumber));
-	            //Print mapped atoms
-	            System.out.println(queryAtom.getSymbol() + " " +
-	                    targetAtom.getSymbol());
+//	            System.out.println(queryMappingNumber + " " +
+//	                    (targetMappingNumber));
+//	            //Print mapped atoms
+//	            System.out.println(queryAtom.getSymbol() + " " +
+//	                    targetAtom.getSymbol());
 				
 			}
-			System.out.println("------- Nr of MCS atoms: " + nrMCSAtoms);
+//			System.out.println("------- Nr of MCS atoms: " + nrMCSAtoms);
 		}
-		System.out.println("+++++++++ Nr of MCS: " + nrMCSs);
+//		System.out.println("+++++++++ Nr of MCS: " + nrMCSs);
+		
 	}
+	
+	
 }
